b5c6715304fc09a44dc9a6ef4ab4d143f197ed6b
Jacek Olszak <jacekolszak@gmail.com>
2022-05-24 12:18:48 +0200

    [k6] Load test inside Docker network
    Docker-proxy is slow and adds significant overhead.

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import exec from 'k6/execution';
 import {check} from 'k6';
 
 export let options = {
-    vus: 5000,
+    vus: 10000,
     duration: "30s",
 };
 
 export default function () {
     let train = exec.vu.idInInstance % 100; // 100 different trains
 	 
-    let res = http.get('http://localhost:8080/book?train=' + train + '&person=1&seat=1');
+    let res = http.get('http://web:8080/book?train=' + train + '&person=1&seat=1');
     check(res, {
         'response code was 200': (res) => res.status == 200,
     });
